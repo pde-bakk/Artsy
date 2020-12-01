c0ff793b5829640985d83472604af217d1cb66aa
Peer de Bakker <pde-bakk@student.codam.nl>
2020-12-01 17:55:32 +0100

    reformated

--- Artsy.py
+++ Artsy.py
@@ -10,28 +10,31 @@
 #                                                                              #
 # **************************************************************************** #
 
-import sys, time, random
+import random
+import sys
+import time
+
 from PIL import Image
-from GenerateASCII import GenerateASCII
+
+from GenerateASCII import generate_ascii
 
 if len(sys.argv) >= 2:
-	if sys.argv[1] == "flex":
-		im = Image.open("imgs/flex.gif")
-	elif sys.argv[1] == "homie":
-		if random.randint(0, 4) == 1:
-			im = Image.open("imgs/christmashomie.gif")
-		else:
-			im = Image.open("imgs/kissahomie.gif")
-	else:
-		im = Image.open("imgs/monkaSTEER.gif")
+    if sys.argv[1] == "flex":
+        im = Image.open("imgs/flex.gif")
+    elif sys.argv[1] == "homie":
+        if random.randint(0, 4) == 1:
+            im = Image.open("imgs/christmashomie.gif")
+        else:
+            im = Image.open("imgs/kissahomie.gif")
+    else:
+        im = Image.open("imgs/monkaSTEER.gif")
 else:
-	im = Image.open("imgs/monkaSTEER.gif")
+    im = Image.open("imgs/monkaSTEER.gif")
 
 print(im.n_frames)
 
 while True:
-	for frame in range(im.n_frames):
-		im.seek(frame)
-		GenerateASCII(im)
-		time.sleep( 2.5 / im.n_frames )
-
+    for frame in range(im.n_frames):
+        im.seek(frame)
+        generate_ascii(im)
+        time.sleep(2.5 / im.n_frames)
