d13061a209aece2ccb6d654b37652f4cb0a98a72
Peer de Bakker <pde-bakk@student.codam.nl>
2020-12-22 01:53:01 +0100

    reformatted some pycharm suggestions

--- GenerateASCII.py
+++ GenerateASCII.py
@@ -32,8 +32,8 @@ def generate_ascii(img):
 	pixels = img.getdata()
 
 	# replace each pixel with a character from array
-	chars = [" ",".",":","!","*","%","$","@","&","#","S"]
-	new_pixels = [chars[pixel//25] for pixel in pixels]
+	chars = [" ", ".", ":", "!", "*", "%", "$", "@", "&", "#", "S"]
+	new_pixels = [chars[pixel // 25] for pixel in pixels]
 	new_pixels = ''.join(new_pixels)
 
 	# split string of chars into multiple strings of length equal to new width and create a list
@@ -41,6 +41,6 @@ def generate_ascii(img):
 	ascii_image = [new_pixels[index:index + new_width] for index in range(0, new_pixels_count, new_width)]
 	ascii_image = "\n".join(ascii_image)
 	print(ascii_image)
-	# write to a text file.
-	# with open("ascii_image.txt", "w") as f:
-	# 	f.write(ascii_image)
+# write to a text file.
+# with open("ascii_image.txt", "w") as f:
+# 	f.write(ascii_image)
