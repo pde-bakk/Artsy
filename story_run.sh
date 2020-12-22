beaaa71ed5d2f8ba9fa8730244d4a0a62234cd6c
Tishj <t_b@live.nl>
2020-12-22 15:40:47 +0100

    changed thor in story

--- story_run.sh
+++ story_run.sh
@@ -4,8 +4,7 @@ python3 artsy.py story/1
 python3 artsy.py story/3
 python3 artsy.py story/2 #covid
 python3 artsy.py story/13 #blackhole
-python3 artsy.py story/5
-python3 artsy.py story/6
+python3 artsy.py story/thor
 python3 artsy.py story/4 #time passes
 python3 artsy.py story/7 #stronger
 python3 artsy.py story/8
